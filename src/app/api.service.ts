0a2fbfa8e1ec3c930fe081c341a53ab4a16c52a4
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-03-22 16:15:13 +0530

    Update api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,21 +3,25 @@ import { Category } from './category';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ApiService {
 	PHP_API_SERVER = "http://localhost/categorymaster";
-	constructor(private httpClient: HttpClient) {}
-	readCategory(): Observable<Category[]>{
+	constructor(private httpClient: HttpClient) { }
+
+	readCategory(): Observable<Category[]> {
 		return this.httpClient.get<Category[]>(`${this.PHP_API_SERVER}/index.php`);
 	}
-	createCategory(category: Category): Observable<Category>{
+
+	createCategory(category: Category): Observable<Category> {
 		return this.httpClient.post<Category>(`${this.PHP_API_SERVER}/create_category.php`, category);
 	}
-	updateCategory(category: Category){
+
+	updateCategory(category: Category) {
 		return this.httpClient.put<Category>(`${this.PHP_API_SERVER}/update_category.php`, category);
 	}
-	deleteCategory(id: number){
+	
+	deleteCategory(id: number) {
 		return this.httpClient.delete<Category>(`${this.PHP_API_SERVER}/delete_category.php/?id=${id}`);
 	}
 }
