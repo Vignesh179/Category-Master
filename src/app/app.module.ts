c070c23ab154ae5cab343871b487d3a38d3a077e
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-03-22 16:15:49 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,HttpClientModule,FormsModule
+    NgbModule, HttpClientModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
