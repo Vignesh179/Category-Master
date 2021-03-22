2102cd160418201eb32ba9214d741748191e8360
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-03-22 12:21:52 +0530

    Final Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { Category } from './category';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'Category-Master';
+    categories!: Category[];
+    selectedCategory: Category = { id : null , name: null, status: null}
+    constructor(private apiService: ApiService) {
+      this.apiService.readCategory().subscribe((categories: Category[])=>{
+      this.categories = categories;
+      console.log(this.categories);
+    }) }
+    ngOnInit()
+    {
+    }
+    createOrUpdateCategory(form:any){
+      form.value.id = this.selectedCategory.id;
+      form.value.name = this.selectedCategory.name;
+      form.value.price = this.selectedCategory.status;
+      if(this.selectedCategory && this.selectedCategory.id){
+        this.apiService.updateCategory(form.value).subscribe((category: Category)=>{
+        console.log("Product updated" , category);
+        this.apiService.readCategory().subscribe((categories: Category[])=>{
+          this.categories = categories;
+        })
+      });
+    }
+    else{
+      this.apiService.createCategory(form.value).subscribe((category: Category)=>{
+        console.log("Product created, ", category);
+        this.apiService.readCategory().subscribe((categories: Category[])=>{
+          this.categories = categories;
+        })
+      });
+    }
+  }
+  
+  selectCategory(category: Category){
+    this.selectedCategory = category;
+  }
+  
+  deleteCategory(id:any){
+    this.apiService.deleteCategory(id).subscribe((category: Category)=>{
+      console.log("Product deleted, ", category);
+      this.apiService.readCategory().subscribe((categories: Category[])=>{
+        this.categories = categories;
+      })
+    });
+  }
+  }
+
