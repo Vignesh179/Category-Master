6597b59b4fc2a05b3af43f98557a99b52506edbc
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-03-23 18:23:38 +0530

    Update api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,9 +8,9 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
 	LOCALHOST = "http://localhost/categorymaster";
 	constructor(private httpClient: HttpClient) { }
-	readCategory(): Observable<Category[]> {
-		return this.httpClient.get<Category[]>(`${this.LOCALHOST}/index.php`);
-	}
+	// readCategory(): Observable<Category[]> {
+	// 	return this.httpClient.get<Category[]>(`${this.LOCALHOST}/index.php`);
+	// }
 	createCategory(category: Category): Observable<Category> {
 		return this.httpClient.post<Category>(`${this.LOCALHOST}/create_category.php`, category);
 	}
@@ -20,9 +20,24 @@ export class ApiService {
 	deleteCategory(id: number) {
 		return this.httpClient.delete<Category>(`${this.LOCALHOST}/delete_category.php/?id=${id}`);
 	}
-	// async get(url: string) {
+	async get(url: string) {
+        try {
+            const resp = await this.httpClient.get(url).toPromise();
+            return resp;
+        } catch (e) {
+            return null;
+        }
+    }
+	// async post(url: string, data?: any) {
     //     try {
-    //         const resp = await this.httpClient.get(url).toPromise();
+            
+
+    //         const resp = await this.httpClient.post(url, data, {
+    //             headers: {
+                    
+    //             },
+    //            // params: data
+    //         }).toPromise();
     //         return resp;
     //     } catch (e) {
     //         return null;
