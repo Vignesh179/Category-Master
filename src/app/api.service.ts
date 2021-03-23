a19f1e39b6d4806f45bf08114754de942551430d
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-03-23 10:33:26 +0530

    Update api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,22 +6,18 @@ import { HttpClient } from '@angular/common/http';
 	providedIn: 'root'
 })
 export class ApiService {
-	PHP_API_SERVER = "http://localhost/categorymaster";
+	LOCALHOST = "http://localhost/categorymaster";
 	constructor(private httpClient: HttpClient) { }
-
 	readCategory(): Observable<Category[]> {
-		return this.httpClient.get<Category[]>(`${this.PHP_API_SERVER}/index.php`);
+		return this.httpClient.get<Category[]>(`${this.LOCALHOST}/index.php`);
 	}
-
 	createCategory(category: Category): Observable<Category> {
-		return this.httpClient.post<Category>(`${this.PHP_API_SERVER}/create_category.php`, category);
+		return this.httpClient.post<Category>(`${this.LOCALHOST}/create_category.php`, category);
 	}
-
 	updateCategory(category: Category) {
-		return this.httpClient.put<Category>(`${this.PHP_API_SERVER}/update_category.php`, category);
+		return this.httpClient.put<Category>(`${this.LOCALHOST}/update_category.php`, category);
 	}
-	
 	deleteCategory(id: number) {
-		return this.httpClient.delete<Category>(`${this.PHP_API_SERVER}/delete_category.php/?id=${id}`);
+		return this.httpClient.delete<Category>(`${this.LOCALHOST}/delete_category.php/?id=${id}`);
 	}
 }
